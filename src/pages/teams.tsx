8f42a5ec8c49d0b292da4d58c9eb7112b3c74dd7
Kolton Musgrove <KoltonGMusgrove@gmail.com>
2023-03-07 15:00:53 -0500

    remove unecessary imports

--- src/pages/teams.tsx
+++ src/pages/teams.tsx
@@ -1,36 +1,37 @@
 import React from "react"
-import {BracketSchema} from "./bracket"
-import { TeamSchema } from "../components/team"
+import { BracketSchema } from "./bracket"
 import { Header } from "../components"
 import { useParams } from "react-router-dom"
 import assert from "assert"
-import { useNavigate, createSearchParams } from "react-router-dom"
 
 export function Teams() {
-    const { id } = useParams()
-    assert.ok(id, "id is required")
+  const { id } = useParams()
+  assert.ok(id, "id is required")
 
-    const stringifiedBracket = localStorage.getItem(id)
-    assert.ok(stringifiedBracket)
+  const stringifiedBracket = localStorage.getItem(id)
+  assert.ok(stringifiedBracket)
 
-    const bracket = JSON.parse(stringifiedBracket) as BracketSchema
-    const bracketTeams = bracket.teams.map((team) =>  <li className="flex justify-center items-center box-border h-12 w-80 p-4 border-4 my-4">{team.name}</li>);
+  const bracket = JSON.parse(stringifiedBracket) as BracketSchema
+  const bracketTeams = bracket.teams.map((team) => (
+    <li className="my-4 box-border flex h-12 w-80 items-center justify-center border-4 p-4">
+      {team.name}
+    </li>
+  ))
 
-    return (
-        <>
-            <Header
-              title={bracket.name}
-              leftIcon={{
-                link: "/brackets/teams",
-                alt: "back",
-                icon: "back",
-            }}
-            />
+  return (
+    <>
+      <Header
+        title={bracket.name}
+        leftIcon={{
+          link: "/brackets/teams",
+          alt: "back",
+          icon: "back"
+        }}
+      />
 
-            <div className="w-screen flex justify-center items-center">
-                <ul>{bracketTeams}</ul>
-            </div>
-            
-        </>
-    )
+      <div className="flex w-screen items-center justify-center">
+        <ul>{bracketTeams}</ul>
+      </div>
+    </>
+  )
 }
