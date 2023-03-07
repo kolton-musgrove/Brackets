03e35da80fbc63b35ded1c8d8e5542fac658143d
Dean Natale <97637351+danatale16@users.noreply.github.com>
2023-03-07 10:59:13 -0500

    Fixed array output

--- src/pages/teams.tsx
+++ src/pages/teams.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import {BracketSchema} from "./bracket"
+import { TeamSchema } from "../components/team"
 import { Header } from "../components"
 import { useParams } from "react-router-dom"
 import assert from "assert"
@@ -13,19 +14,23 @@ export function Teams() {
     assert.ok(stringifiedBracket)
 
     const bracket = JSON.parse(stringifiedBracket) as BracketSchema
+    const bracketTeams = bracket.teams.map((team) =>  <li className="flex justify-center items-center box-border h-12 w-80 p-4 border-4 my-4">{team.name}</li>);
 
     return (
         <>
             <Header
-              title={id}
+              title={bracket.name}
               leftIcon={{
                 link: "/brackets/teams",
                 alt: "back",
-                icon: "back"
+                icon: "back",
             }}
             />
 
-
+            <div className="w-screen flex justify-center items-center">
+                <ul>{bracketTeams}</ul>
+            </div>
+            
         </>
     )
 }
